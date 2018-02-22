6935f72ba3996f54f65b5a30cd770860fc7870d9
Tinashe Chihoro <tinashechihoro@gmail.com>
2018-02-22 15:00:14 +0200

    plugin _load method

---
+++ manager.py
@@ -0,0 +1,21 @@
+class PluginManager():
+    def __init__(self.path=None,plugin_init_args={}):
+        if path:
+            self.plugin_dir = path
+        else:
+            self.plugin_dir = os.path.dirname(__file__) + '/plugins/'
+        self.plugins = []
+        self._load_plugins()
+        self._register_plugins(**plugin_init_args)
+
+    def _load_plugins(self):
+        sys.path.append(plugin_dir)
+        plug_files = [file  for file in os.listdir(self.plugin_dir) if file.startswith('plugin_') and file.endswith('.py') ]
+        plugin_modules = [ module.split('.')[0] for module in plug_files ]
+        for module in plugin_modules:
+            module = __import__(module)
+    
+    def __register_plugins(self,**kwargs):
+        for plugin in Plugins.__subclasses__():
+            obj = plugin(**kwargs)
+            self.plugins.append(obj)
